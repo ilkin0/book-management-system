7e67ff1e9d5060fc15ee6a1a0a38797ab5684b25
Ilkin Mehdiyev <mehdiyevilkin01@gmail.com>
2022-06-16 23:13:54 +0400

    Added some config files, ms-user-management basics, etc.

---
+++ ms-user-management/src/main/java/com/ilkinmehdiyev/usermanagement/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.ilkinmehdiyev.usermanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class TestController {
+    @GetMapping("hello")
+    public String hello(){
+        return "Hello";
+    }
+}
