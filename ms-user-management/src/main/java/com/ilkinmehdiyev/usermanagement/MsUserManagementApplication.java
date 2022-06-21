6cd20d9c95670362b3c6f196715ff05b3aae5bba
Ilkin Mehdiyev <hey@ilkinmehdiyev.com>
2022-06-21 23:23:21 +0400

    WIP setting Spring Cloud Config server

--- ms-user-management/src/main/java/com/ilkinmehdiyev/usermanagement/MsUserManagementApplication.java
+++ ms-user-management/src/main/java/com/ilkinmehdiyev/usermanagement/MsUserManagementApplication.java
@@ -2,8 +2,11 @@ package com.ilkinmehdiyev.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
+//        (exclude={DataSourceAutoConfiguration.class})
 public class MsUserManagementApplication {
     public static void main(String... args) {
         SpringApplication.run(MsUserManagementApplication.class);
