41692237d842c8397b5e466a58769e0cdc9f1a20
Ilkin Mehdiyev <hey@ilkinmehdiyev.com>
2022-10-14 00:22:22 +0400

    WIP init ms-book-management

---
+++ ms-book-management/src/main/java/com/ilkinmehdiyev/bookmanagement/MsBookManagementApplication.java
@@ -0,0 +1,11 @@
+package com.ilkinmehdiyev.bookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBookManagementApplication {
+    public static void main(String... args) {
+        SpringApplication.run(MsBookManagementApplication.class, args);
+    }
+}
